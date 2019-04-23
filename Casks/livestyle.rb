eeabae6e29ab8f8d5d1a6e7ab9a2c699af51e991
Randall <ran.dall@icloud.com>
2019-04-23 10:15:03 -0700

    Initial commit
    This is where is all began…
    
    Signed-off-by: Randall <ran.dall@icloud.com>

---
+++ Casks/livestyle.rb
@@ -0,0 +1,12 @@
+cask 'livestyle' do
+  version '1.0.0'
+  sha256 'ead74c18fe505610a04ffad63aa8a5365f456b92f8d784d9ed15d937c90a1561'
+
+  # github.com/livestyle/app was verified as official when first introduced to the cask
+  url "https://github.com/livestyle/app/releases/download/v#{version}/livestyle-osx.zip"
+  appcast 'https://github.com/livestyle/app/releases.atom'
+  name 'LiveStyle'
+  homepage 'http://livestyle.io/'
+
+  app 'LiveStyle.app'
+end
