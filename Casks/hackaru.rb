f49e17757974bfb27f3bcbfa6ca02a0a6251a7bf
Randall <ran.dall@icloud.com>
2019-07-15 15:47:55 -0700

    Cask Style Update

--- Casks/hackaru.rb
+++ Casks/hackaru.rb
@@ -1,19 +1,19 @@
-cask 'hackaru' do
-  version '0.1.0'
-  sha256 '216a1baa87c5bcc65c84cd569cf492286d910236b7708a3caf6847556dc857df'
+cask "hackaru" do
+  version "0.1.0"
+  sha256 "216a1baa87c5bcc65c84cd569cf492286d910236b7708a3caf6847556dc857df"
 
   url "https://github.com/ktmouk/hackaru-desktop/releases/download/v#{version}/Hackaru-#{version}.dmg"
-  appcast 'https://github.com/ktmouk/hackaru-desktop/releases'
-  name 'Hackaru'
-  homepage 'https://github.com/ktmouk/hackaru-desktop'
+  appcast "https://github.com/ktmouk/hackaru-desktop/releases"
+  name "Hackaru"
+  homepage "https://github.com/ktmouk/hackaru-desktop"
 
-  app 'Hackaru.app'
+  app "Hackaru.app"
 
-  uninstall delete: [
-                      '~/Library/Application\ Support/Hackaru',
-                      '~/Library/Saved\ Application\ State/app.hackaru.savedState/',
-                      '~/Library/Preferences/app.hackaru.helper.plist',
-                      '~/Library/Preferences/app.hackaru.plist',
-                      '~/Library/Logs/Hackaru/',
-                    ]
+  uninstall :delete => [
+    '~/Library/Application\ Support/Hackaru',
+    '~/Library/Saved\ Application\ State/app.hackaru.savedState/',
+    "~/Library/Preferences/app.hackaru.helper.plist",
+    "~/Library/Preferences/app.hackaru.plist",
+    "~/Library/Logs/Hackaru/",
+  ]
 end
