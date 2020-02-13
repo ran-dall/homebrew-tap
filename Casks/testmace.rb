e8d37b3ea9f0c10217f7b8fd0d8918201ff8cb70
Randall <ran.dall@icloud.com>
2020-02-13 03:02:30 -0800

    Update testmace from 0.0.1-beta.12 to 0.0.1-beta.29

--- Casks/testmace.rb
+++ Casks/testmace.rb
@@ -1,17 +1,17 @@
-cask "testmace" do
-  version "0.0.1-beta.12"
-  sha256 "0cb55918ca62c11cd52123e0ab64416909bd30683f3171a6c08ada0b091011b6"
+cask 'testmace' do
+  version '0.0.1-beta.29'
+  sha256 'c81f31d3bcb6d8334a3e7aa6d66eedbb83e9a5305efe47a0d2a398af80b45ed8'
 
-  # s3.amazonaws.com/testmacedownload was verified as official when first introduced to the cask
-  url "https://s3.amazonaws.com/testmacedownload/TestMace.dmg"
-  name "TestMace"
-  homepage "https://testmace.com/"
+  url 'https://download.testmace.com/TestMace.dmg'
+  appcast 'https://testmace.com/blog/'
+  name 'TestMace'
+  homepage 'https://testmace.com/'
 
-  app "TestMace.app"
+  app 'TestMace.app'
 
-  uninstall :delete => [
-    '~/Library/Application\ Support/TestMace',
-    '~/Library/Application\ Support/com.electron.testmace.plist',
-    "~/Library/Logs/TestMace",
-  ]
+  zap trash: [
+               '~/Library/Application\ Support/TestMace',
+               '~/Library/Application\ Support/com.electron.testmace.plist',
+               '~/Library/Logs/TestMace',
+             ]
 end
